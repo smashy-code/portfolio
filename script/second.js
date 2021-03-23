0cddd2626567b9e67bf65f9addbec92bbcdb8b2a
smashy-code <73206032+smashy-code@users.noreply.github.com>
2021-03-24 08:41:33 +0900

    Add files via upload

---
+++ script/second.js
@@ -0,0 +1,30 @@
+const slider2 = new Swiper('.slider2', {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  slidesPerGroup: 2,
+  speed: 1500,
+  loop: true,
+  
+  breakpoints: {
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      slidesPerGroup: 3,
+      speed: 1500,
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: -30,
+      speed: 1500,
+      slidesPerGroup: 4,
+      loop: true,    
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
